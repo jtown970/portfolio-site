7bd2237609b6454b139c94ceb0110230f6162253
jtown970 <jasonrtowner@gmail.com>
2020-08-01 20:01:07 -0700

    changed wording on about, changed about to space-between

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import {Link, withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
-// import myRes from '../img/myResume.PNG'
-import myResume from '../img/cleanResume.PNG'
+import myResume from '../img/smaller-resume.PNG'
 import codePound from '../img/code-pound-home.PNG'
 import iVote from '../img/I-vote.PNG'
  const Welcome = () => {
@@ -15,7 +14,6 @@ import iVote from '../img/I-vote.PNG'
   const [passion, setPassion] = useState(false)
   const [goals, setGoals] = useState(false)
   const [aboutSpanInput, setAboutSpan] = useState('Story')
-  const [seeResume, setSeeResume] = useState(false)
 
 
     // pop up
@@ -167,11 +165,11 @@ function closeModal(modal) {
         <div className="about-info">
           <p className="about-text">My <span className="span-login">{aboutSpanInput}</span></p>
           { story === true ? (
-          <p className="about-bio">Hey my name is Jason Towner I'm a 26 year old Full Stack web developer. <br/>I made the switch to programing when I was 24 after working in restaurants for 13 yep started bussing table at 11.  I feel in love with coding when I first saw that I could build something that would alow people from all over the world to use something I was able to build.</p>
+          <p className="about-bio">I am Jason Towner and I'm a Full Stack web developer.I started programming after 13 years in the restaurant business (yes, I started bussing tables at age 11).  Creating an experience in the food industry is limited to the people who come into your restaurant.  With coding and web development, what you create can be experienced by people worldwide.  That ability to influence without boundaries is what keeps me passionate about coding.  I am hoping to join a company to create something bigger than myself and help make an impact on a larger level.</p>
           ) : ( passion === true ? (
-            <p className="about-bio">While there are many things I would say I'm passionate about the main three would be programming, photography, and the outdoors.  Programming has changed my life in many ways it sparks my curiosity, improved my patience, and opened up a new carer path. photography and the outdoors go hand in hand.  I had the opportunity to adventure to some amazing places over the years and the camera allows to me capture that moment in time to look back on and enjoy and sure with friends and family. </p>
+            <p className="about-bio">The top three things I am passionate about are programming, photography, and the outdoors. Programming has changed my life in many ways. It sparked my curiosity, improved my patience, and opened up a new career path.  Photography and the outdoors go hand in hand. I have had the opportunity to create some epic adventures in some of the most beautiful places.  The camera allows to me capture those moments in time and then look back and enjoy and share with friends and family. </p>
           ) : ( goals === true ? (
-            <p className="about-bio"> My goals are separated into three areas yet all overlap. starting with work, I aim not to land a great job but to begin a great career, one that I'm confident that what I'm doing on a daily basis adds up to make positive impact.  Heath, I aim to hike every 14er in colorado, this goal was set after reacting my first summit over 14k feet and continues after reacting my 14th.  Happiness is a harder one to pin down, but hearing your hired from you would more likely help. haha </p>
+            <p className="about-bio">My goals are separated into three areas that all overlap.<br/>Work: I aim not to land a great job but to begin a great career, one that I can feel confident that what I'm doing on a daily basis adds up to make positive impact.<br/>Heath: I aim to hike every 14er in Colorado. This goal was set after reaching my first summit over 14k feet.  I currently done 14 14ers and not ready to stop any time soon.<br/>Happiness: That is a harder one to pin down, but hearing “Your hired” from you would more likely help. haha </p>
           ): null ))}
         </div>
         {/* <div className="tech-sections"> */}
